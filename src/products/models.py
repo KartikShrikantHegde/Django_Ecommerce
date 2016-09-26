58dc682d92f7f94f3ec8434fe43644300e18816d
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-09-26 02:34:00 -0500

    Adding product manager through queryset  concept

--- src/products/models.py
+++ src/products/models.py
@@ -1,17 +1,34 @@
 from django.core.urlresolvers import reverse
 from django.db import models
 
+
 # Create your models here.
+
+class ProductQuerySet(models.query.QuerySet):
+    def active(self):
+        return self.filter(active=True)
+
+
+class ProductManager(models.Manager):
+    def get_queryset(self):
+        return ProductQuerySet(self.model, using=self._db)
+
+    # this overrides the built in all method
+
+    def all(self, *args, **kwargs):
+        return self.get_queryset().active()
+
 
 class Product(models.Model):
     title = models.CharField(max_length=120)
-    description = models.TextField(blank=True,null=True)
+    description = models.TextField(blank=True, null=True)
     price = models.DecimalField(decimal_places=2, max_digits=20)
     active = models.BooleanField(default=True)
 
+    objects = ProductManager()
 
     def __unicode__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse("product_detail", kwargs={"pk":self.pk})
+        return reverse("product_detail", kwargs={"pk": self.pk})
