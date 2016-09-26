62814100353742abdb6ca43adbde60e5cd880305
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-09-26 03:11:05 -0500

    Registering a new model as separate site

--- src/products/admin.py
+++ src/products/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Product
+from .models import Product,Variations
 
 admin.site.register(Product)
+
+admin.site.register(Variations)
