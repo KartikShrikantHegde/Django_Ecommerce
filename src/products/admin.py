814238522410b63c73afe3343335caba8afde96c
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-09-29 04:36:19 -0500

    Image upload class sire register

--- src/products/admin.py
+++ src/products/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Product,Variations
+from .models import Product,Variations, ProductImage
 
 admin.site.register(Product)
 
 admin.site.register(Variations)
+
+admin.site.register(ProductImage)
