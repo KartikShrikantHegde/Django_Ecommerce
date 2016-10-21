8f594cc4238733bd89be74e3ddbff02f11b4c345
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-10-20 21:22:35 -0500

    Display the related products

--- src/products/views.py
+++ src/products/views.py
@@ -107,6 +107,12 @@ class ProductListView(ListView):
 class ProductDetailView(DetailView):
     model = Product
 
+    # override the default get contect method
+    def get_context_data(self,*args, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(*args,**kwargs)
+        instance = self.get_object()
+        context["related"] = Product.objects.get_related(instance).order_by("?")[:6]   # list only 6 related products
+        return context
 
 def product_detail_view_func(request, id):
     # product_instance = Product.objects.get(id=id)
