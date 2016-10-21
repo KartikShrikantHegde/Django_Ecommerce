d8bef0d4dea2a276574413f18f9e6a2b2eacaf8b
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-10-20 19:49:08 -0500

    Adding categories and related foreign keys to models and register it in admin

--- src/products/admin.py
+++ src/products/admin.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Product,Variations, ProductImage
+from .models import Product,Variations, ProductImage,Category
 
 admin.site.register(Product)
 
 admin.site.register(Variations)
 
 admin.site.register(ProductImage)
+
+admin.site.register(Category)
--- src/products/models.py
+++ src/products/models.py
@@ -26,6 +26,8 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(decimal_places=2, max_digits=20)
     active = models.BooleanField(default=True)
+    categories = models.ManyToManyField('Category', blank=True)
+    default = models.ForeignKey('Category',related_name='default_category', null=True, blank=True)
 
     objects = ProductManager()
 
@@ -92,3 +94,17 @@ class ProductImage(models.Model):
 
     def __unicode__(self):
         return self.product.title
+
+
+# Product category
+
+class Category(models.Model):
+    title = models.CharField(max_length=120, unique=True)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(blank=True, null=True)
+    active = models.BooleanField(default=True)
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def __unicode__(self):
+        return self.title
+
