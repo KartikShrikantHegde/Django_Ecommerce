5288da41ee1c3726e0f07757db3c590189657ee8
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-10-25 01:42:15 -0500

    Adding featured and recommended products to home page

--- src/products/admin.py
+++ src/products/admin.py
@@ -2,13 +2,40 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Product,Variations, ProductImage,Category,ProductFeatured
+from .models import Product, Variations, ProductImage, Category, ProductFeatured
 
-admin.site.register(Product)
 
-admin.site.register(Variations)
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 0  # Just one inline field not more
+    max_num = 10
 
-admin.site.register(ProductImage)
+
+class VariationInline(admin.TabularInline):
+    model = Variations
+    extra = 0
+    max_num = 10  # The no variations allowed per product
+
+
+# To manage products in admin page
+class ProductAdmin(admin.ModelAdmin):
+    # Here unicode represents the product itself, price ll be shown against it in admin page
+    list_display = ['__unicode__', 'price']
+    inlines = [
+        ProductImageInline,
+        VariationInline,
+    ]
+
+    class Meta:
+        model = Product
+
+
+admin.site.register(Product, ProductAdmin)
+
+# this can be commented as variation is added as model in inline
+# admin.site.register(Variations)
+
+# admin.site.register(ProductImage)
 
 admin.site.register(Category)
 
