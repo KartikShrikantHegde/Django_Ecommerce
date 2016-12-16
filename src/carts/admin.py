50709ac3551e8db3bbd6162540d91734e0c42466
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-12-16 01:56:26 -0600

    Inline models and adding them to main cart

--- src/carts/admin.py
+++ src/carts/admin.py
@@ -4,16 +4,20 @@ from django.contrib import admin
 
 from .models import Cart, CartItem
 
+
+# Make the cart items table tabular
 class CartItemInline(admin.TabularInline):
     model = CartItem
 
+
+# define the inliners for the admin
 class CartAdmin(admin.ModelAdmin):
     inlines = [
         CartItemInline
     ]
+
     class Meta:
         model = Cart
 
 
-admin.site.register(Cart,CartAdmin)
-
+admin.site.register(Cart, CartAdmin)
