1d1c10996f9da41ee9d129f9e9f1caedb8f6a0a1
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-12-19 01:30:08 -0600

    Inline models and adding them to main cart

--- src/carts/views.py
+++ src/carts/views.py
@@ -1,3 +1,12 @@
+from django.views.generic.base import View
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+
+class CartView(View):
+    def get(self,request,*args,**kwargs):
+        item = request.GET.get("item")
+        qty = request.GET.get("qty")
+        print item,qty
+        return HttpResponseRedirect("/")
