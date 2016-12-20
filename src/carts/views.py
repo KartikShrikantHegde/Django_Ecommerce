af0e61ed26e25ff9ca4518a918b10036114704c5
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-12-19 18:20:47 -0600

    CartView for get requests

--- src/carts/views.py
+++ src/carts/views.py
@@ -1,12 +1,25 @@
 from django.views.generic.base import View
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
+from products.models import Variations
+from carts.models import Cart, CartItem
+
+
 class CartView(View):
-    def get(self,request,*args,**kwargs):
-        item = request.GET.get("item")
-        qty = request.GET.get("qty")
-        print item,qty
+    def get(self, request, *args, **kwargs):
+        item_id = request.GET.get("item")
+        delete_item = request.GET.get("delete")
+        if item_id:
+            item_instance = get_object_or_404(Variations, id=item_id)
+            qty = request.GET.get("qty")
+            cart = Cart.objects.all().first()
+            cart_item = CartItem.objects.get_or_create(cart=cart,item = item_instance)[0]
+            if delete_item:
+                cart_item.delete()
+            else:
+                cart_item.quantity = qty
+                cart_item.save()
         return HttpResponseRedirect("/")
